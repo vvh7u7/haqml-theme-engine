1bd821ece3f4b3fead2a17e4bbacfdc08dabdc53
Vladyslav Harbar <vvh7u7@gmail.com>
2026-06-11 08:40:41 +0200

    Documented the IconManager.cpp

--- module/source/IconManager.cpp
+++ module/source/IconManager.cpp
@@ -6,12 +6,15 @@
 namespace ThemeEngine {
 
 ThemeEngine::IconManager::IconManager(QObject *parent) {
+    // By default, icons are looked up in the application launch folder: /assets/icons/material/
     m_defaultIconsPath = QCoreApplication::applicationDirPath() + "/assets/icons/material/";
 }
 
 QString IconManager::getIcon(const QString &iconName) const {
+     // Automatically append the .svg extension if not specified by the user
     QString svgName = iconName.endsWith(".svg") ? iconName : iconName + ".svg";
 
+    // First, check if the icon exists in the custom directory
     if (!m_customIconsPath.isEmpty()) {
         QFile customFile(m_customIconsPath + svgName);
         if (customFile.exists()) {
@@ -19,6 +22,7 @@ QString IconManager::getIcon(const QString &iconName) const {
         }
     }
 
+    // If not found in custom, search the default directory
     QFile defaultFile(m_defaultIconsPath + svgName);
     if (!defaultFile.exists()) {
         return QUrl::fromLocalFile(defaultFile.fileName()).toString();
